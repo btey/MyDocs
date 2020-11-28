4719e03f5b12ad64d03f05fb2a0af57e82281f46
Tey, Benjamin <bentey@bethel.jw.org>
2020-11-28 21:23:11 +0100

    Init version

---
+++ blog/2020-11-17-CentOS8-OpenProject-EE.md
@@ -0,0 +1,69 @@
+# OpenProject (Docker) + CentOS 8
+
+Note: start with a CentOS 8 server installation without any extras.
+
+## Enable EPEL and PowerTools repository for CentOS 8
+
+To enable EPEL Repo use the following command:
+
+```bash
+dnf install epel-release
+```
+
+To enable PowerTools Repo use the folowing command:
+
+```bash
+dnf config-manager --set-enabled PowerTools
+```
+
+## Install Podman
+
+First update your system:
+
+```bash
+dnf -y update
+systemctl reboot
+```
+
+Check the available `container-tools` packages:
+
+```bash
+dnf module list  grep container-tools
+```
+
+```bash
+container-tools    rhel8 [d][e]   common [d]    Common tools and dependencies for container runtimes
+container-tools    1.0            common [d]    Common tools and dependencies for container runtimes
+container-tools    2.0            common [d]    Common tools and dependencies for container runtimes
+```
+
+Install Podman with the following command:
+
+```bash
+dnf install -y container-tools
+```
+
+Check the `podman` version available:
+
+```bash
+podman version
+```
+
+```bash
+Version:            1.6.4
+RemoteAPI Version:  1
+Go Version:         go1.13.4
+OS/Arch:            linux/amd64
+```
+
+## Install Python 3 and git
+
+```bash
+dnf install python38 git
+```
+
+## Install `podman-compose`
+
+```bash
+pip3 install podman-compose
+```
---
+++ docusaurus.config.js
@@ -0,0 +1,179 @@
+module.exports = {
+  title: 'My Site',
+  tagline: 'The tagline of my site',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themes: ['@docusaurus/theme-live-codeblock'],
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'community',
+        path: 'community',
+        editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+        routeBasePath: 'community',
+        sidebarPath: require.resolve('./sidebarsCommunity.js'),
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-blog',
+      {
+        id: 'second-blog',
+        path: 'second-blog/blog',
+        routeBasePath: 'second-blog',
+        editUrl:
+          'https://github.com/facebook/docusaurus/edit/master/website/dogfooding',
+        postsPerPage: 3,
+        feedOptions: {
+          type: 'all',
+          copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc.`,
+        },
+      },
+    ],
+    [
+      '@easyops-cn/docusaurus-search-local',
+      {
+        hashed: true,
+        docsDir: [
+          "docs", 
+          "community"
+        ],
+        docsRouteBasePath: [
+          "/docs",
+          "/community"
+        ],
+        blogDir: [
+          "blog",
+          "second-blog/blog"
+        ],
+        blogRouteBasePath: [
+          "/blog",
+          "/second-blog"
+        ],
+      },
+    ],
+  ],
+  themeConfig: {
+    hideableSidebar: true,
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      hideOnScroll: false,
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+        srcDark: 'img/docusaurus_keytar.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          //activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+          docId: 'introduction',
+        },
+        {
+          to: '/community/support',
+          label: 'Community',
+          position: 'left',
+          activeBaseRegex: `/community/`,
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {to: 'second-blog', label: 'Blog 2', position: 'left'},
+        //{
+        //  href: 'https://github.com/facebook/docusaurus',
+        //  label: 'GitHub',
+        //  position: 'right',
+        //},
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: 'docs',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          path: 'blog',
+          postsPerPage: 3,
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          feedOptions: {
+            type: 'all',
+            copyright: `Copyright © ${new Date().getFullYear()} Facebook, Inc.`,
+          },
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'All our posts',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
